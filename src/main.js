ee715b302bd519bdb2e8d3b72393e4f8e2cfd04f
i5ar <i5ar@live.com>
2019-04-18 13:58:58 +0200

    Fix API state

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      api: false,
+      api: true,
       keyboards: [],
       info: {},
       keyboard: "",
