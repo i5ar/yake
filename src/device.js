2f33a64c23ed94e9687e096a8aa4726c7937d391
unknown <i5ar@live.it>
2021-01-23 13:50:23 +0100

    add xmlns attribute to open image in the browser

--- src/device.js
+++ src/device.js
@@ -186,6 +186,7 @@ export default class Device extends React.Component {
       },
       e(
         "svg", {
+          xmlns: "http://www.w3.org/2000/svg",
           width,
           height,
           viewBox: `0 0 ${width} ${height}`,
